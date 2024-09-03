2b7d6118ecc247acd67340233e49ef0115dac058
Diep Chu <chuvandiep2804@gmail.com>
2024-09-03 22:09:06 +0700

    #65

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -25,16 +25,25 @@ public class UserController {
         return "hello";
     }
 
+    // Get List Data
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
+        List<User> users = this.userService.getAllUsers();
+        model.addAttribute("users", users);
+        System.out.println("list user : " + users);
+        return "admin/user/table-user";
+    }
+
+    @RequestMapping("/admin/user/create")
+    public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
 
-    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanIT) {
         System.out.println("run here" + hoidanIT);
         this.userService.handleSaveUser(hoidanIT);
-        return "hello";
+        return "redirect:/admin/user";
     }
 }
